906ea7470eef4d3aa11b3bbafe692d1113f576dc
MuhaabDev <mohabjr111@gmail.com>
2024-12-04 12:38:09 +0200

    only one message

--- index.java
+++ index.java
@@ -5,6 +5,5 @@ public class Main {
 
         String x = JOptionPane.showInputDialog("HOW R YOU");
 
-        JOptionPane.showMessageDialog(null , "hello world" , "greeting" , JOptionPane.OK_CANCEL_OPTION);
     }
 }
