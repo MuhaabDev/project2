8a0d234104ca139d081c6e1ef3598820f9edaeb1
MuhaabDev <mohabjr111@gmail.com>
2024-12-04 12:33:36 +0200

    creating hello

---
+++ index.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String x = JOptionPane.showInputDialog("HOW R YOU");
+
+        JOptionPane.showMessageDialog(null , "hello world" , "greeting" , JOptionPane.OK_CANCEL_OPTION);
+    }
+}
